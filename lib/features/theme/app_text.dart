8fd213e07f1871c472419b142593c734993151e0
MatheusFontenele <matheus.fontenele@icloud.com>
2023-09-29 23:56:55 -0300

    refactor: refactondo app text e colors

--- lib/features/theme/app_text.dart
+++ lib/features/theme/app_text.dart
@@ -45,12 +45,6 @@ class AppText {
     color: AppColors.primary,
   );
 
-  static final titleListTile = GoogleFonts.lexendDeca(
-    fontSize: 17,
-    fontWeight: FontWeight.w600,
-    color: AppColors.heading,
-  );
-
   static final trailingRegular = GoogleFonts.mulish(
     fontSize: 16,
     fontWeight: FontWeight.w400,
@@ -61,96 +55,6 @@ class AppText {
     fontSize: 16,
     fontWeight: FontWeight.bold,
     color: AppColors.heading,
-  );
-
-  static final buttonPrimary = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    color: AppColors.primary,
-  );
-
-  static final buttonHeading = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    color: AppColors.heading,
-  );
-
-  static final buttonGrey = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    color: AppColors.grey,
-  );
-
-  static final buttonBackground = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    color: AppColors.background,
-  );
-
-  static final buttonBoldPrimary = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w700,
-    color: AppColors.primary,
-  );
-
-  static final buttonBoldHeading = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w700,
-    color: AppColors.heading,
-  );
-
-  static final buttonBoldGrey = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w700,
-    color: AppColors.grey,
-  );
-
-  static final buttonBoldBackground = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w700,
-    color: AppColors.heading,
-  );
-
-  static final input = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    color: AppColors.input,
-  );
-
-  static final captionBackground = GoogleFonts.poppins(
-    fontSize: 18,
-    fontWeight: FontWeight.w400,
-    color: AppColors.text,
-  );
-
-  static final captionShape = GoogleFonts.inter(
-    fontSize: 18,
-    fontWeight: FontWeight.w400,
-    color: AppColors.shape,
-  );
-
-  static final captionBody = GoogleFonts.inter(
-    fontSize: 13,
-    fontWeight: FontWeight.w400,
-    color: AppColors.body,
-  );
-
-  static final captionBoldBackground = GoogleFonts.poppins(
-    fontSize: 18,
-    fontWeight: FontWeight.w600,
-    color: AppColors.heading,
-  );
-
-  static final captionBoldShape = GoogleFonts.inter(
-    fontSize: 13,
-    fontWeight: FontWeight.w600,
-    color: AppColors.shape,
-  );
-
-  static final captionBoldBody = GoogleFonts.inter(
-    fontSize: 13,
-    fontWeight: FontWeight.w600,
-    color: AppColors.body,
   );
 
   static final cardBoldTitle = GoogleFonts.mulish(
@@ -169,11 +73,5 @@ class AppText {
     fontSize: 24,
     fontWeight: FontWeight.normal,
     color: AppColors.background,
-  );
-
-  static final buttonGray = GoogleFonts.inter(
-    fontSize: 15,
-    fontWeight: FontWeight.w400,
-    color: AppColors.primary,
   );
 }
