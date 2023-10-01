029a3c9729db2b433467db04da75f6ace1d8db60
MatheusFontenele <matheus.fontenele@icloud.com>
2023-09-30 22:42:46 -0300

    feature: iniciando a splash screen

--- lib/features/auth/presenter/screens/pages/splash/splash_page.dart
+++ lib/features/auth/presenter/screens/pages/splash/splash_page.dart
@@ -10,28 +10,6 @@ class SplashPage extends StatelessWidget {
     // final authController = AuthController();
     // authController.currentUser(context);
     return const Scaffold(
-      backgroundColor: AppColors.background,
-      body: Stack(
-          // children: [
-          //   Center(
-          //     child: Image.asset(AppImage.union),
-          //   ),
-          //   Center(
-          //     child: Column(
-          //       mainAxisAlignment: MainAxisAlignment.center,
-          //       children: [
-          //         // Padding(
-          //         //   padding: const EdgeInsets.only(bottom: 20),
-          //         //   child: Image.asset(AppImage.codeBar),
-          //         // ),
-          //         // Center(
-          //         //   child: Image.asset(AppImage.logo),
-          //         // )
-          //       ],
-          //     ),
-          //   ),
-          // ],
-          ),
-    );
+        backgroundColor: AppColors.background, body: Placeholder());
   }
 }
